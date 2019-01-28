307587c340a1cd9bcd4bba48e69b456bbdba9ff3
Mridul Malviya <mridul.malviya@gmail.com>
2019-01-28 21:51:28 +0530

    Add project files.

---
+++ MasterPageDemo/MasterPageDemo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MasterPageDemo.Models;
+
+namespace MasterPageDemo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
